f3bba2a4042fdd3c9af3fcd8243eb6b81aaf1faa
Justin Johnson <johnsonj561@gmail.com>
2017-03-30 23:22:36 -0400

    reset password functionality fixed

--- public/app/services/userServices.js
+++ public/app/services/userServices.js
@@ -1,6 +1,6 @@
 angular.module('userServices', [])
 
-.factory('User', function($http) {
+  .factory('User', function($http) {
   var userFactory = {}; // Create the User object
 
   userFactory.create = function(regData) {
@@ -29,25 +29,25 @@ angular.module('userServices', [])
     return $http.put('/api/resend', username)
   };
 
-  // User.sendUsername(userData);
+  // Send user's username to e-mail
   userFactory.sendUsername = function(userData) {
     return $http.get('/api/resetusername/' + userData);
   };
 
-  // User.sendPassword(resetData)
+  // Send password reset link to user's e-mail
   userFactory.sendPassword = function(resetData) {
-    return $http.put('/api/resetpassword', resetData);  
+    return $http.put('/api/resetpassword', resetData);
   };
 
-  // User.resetUser(token);
+  // Grab user's information from e-mail reset link
   userFactory.resetUser = function(token) {
-    return $http.get('/resetpassword/' + token);  
+    return $http.get('/api/resetpassword/' + token);
   };
 
-  // User.savePassword(regData)
+  // Save user's new password
   userFactory.savePassword = function(regData) {
-    return $http.put('/api/savepassword', regData)  
-  };
+    return $http.put('/api/savepassword', regData)
+  }
 
   userFactory.renewSession = function(username) {
     return $http.get('/api/renewToken/' + username);  
@@ -60,19 +60,19 @@ angular.module('userServices', [])
   userFactory.getUsers = function() {
     return $http.get('/api/management/');
   };
-  
+
   userFactory.getUser = function(id) {
     return $http.get('/api/edit/' + id);  
   };
-  
+
   userFactory.deleteUser = function(username) {
     return $http.delete('/api/management/' + username);  
   };
-  
+
   userFactory.editUser = function(id) {
     return $http.put('/api/edit/', id);
   };
-  
+
   return userFactory;
 });
 
