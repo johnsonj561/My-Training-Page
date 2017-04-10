78c21e44ba21c01c9030b12f17b9302cfb32822f
Justin Johnson <johnsonj561@gmail.com>
2017-04-09 21:33:42 -0400

    removed trainingcomponent schema type temporary work around

--- app/models/trainingmodule.js
+++ app/models/trainingmodule.js
@@ -3,6 +3,25 @@ var Schema = mongoose.Schema;
 
 
 /*
+* Individual Training Component Schema
+* No specific format is required to allow user to choose between various component types
+CURRENTLY NOT BEING USED
+var TrainingComponent = new Schema({
+  pageType : { type: String, required: true },
+  title: { type: String, required: false },
+  body: { type: String, required: false },
+  video: { type: String, required: false },
+  audio: { type: String, required: false },
+  description: { type: String, required: false },
+  question: { type: String, required: false },
+  answer: { type: Number, required: false }
+});
+
+*/
+
+
+
+/*
 * Training Module Schema
 */
 var TrainingModuleSchema = new Schema({
@@ -11,14 +30,14 @@ var TrainingModuleSchema = new Schema({
     _id: { type: Schema.Types.ObjectId, required: true },
     name: {type: String, required: true }
   },
-  last_edit: { type : Date, required: true, default: Date.now },
+  lastEdit: { type : Date, required: true, default: Date.now },
   components: {type: Array, required: true },
   assignedCount: { type: Number, required: true, default: 0 },
   completedCount: { type: Number, required: true, default: 0 },
-  totlaScores: { type: Number, required: true, default: 0 },
+  totalScores: { type: Number, required: true, default: 0 },
   lowScore: { type: Number, required: true, default: 0 },
   highScore: { type: Number, required: true, default: 0 }
-}, {collection: 'training'});
+}, {collection: 'trainingmodules'});
 
 
 module.exports = mongoose.model('TrainingModule', TrainingModuleSchema);
