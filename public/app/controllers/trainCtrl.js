32e39859fae6537c971bdf74e94e76e7f2c4670d
Justin Johnson <johnsonj561@gmail.com>
2017-04-09 21:34:10 -0400

    media component functionality added

--- public/app/controllers/trainCtrl.js
+++ public/app/controllers/trainCtrl.js
@@ -1,32 +1,51 @@
 angular.module('trainController', ['trainingServices'])
 
-  .controller('trainCtrl', function(TrainingModule, $location) {
+  .controller('trainCtrl', function(TrainingModule, $location, $scope) {
 
   app = this;  
 
+  
+  /*
+  * Load Media
+  * Checks component for media type and loads into source
+  */
+  app.loadMedia = function(component) {
+    if(component.pageType == 'video') {
+      console.log('video type detected');
+      app.component.source = component.video;
+    }
+    else if(component.pageType == 'audio') {
+      console.log('audio type detected');
+      app.component.source = component.audio;
+    }
+  }
 
   // get list of training modules registered to current user
   // display in drop down menu for selection ?
   // when selected, submit the training module _id to server and return the training module
 
-  var training_id =  '58e917e69d5382072055aa8c';
+  var training_id =  '58ead365f026bb6d79240594';
+
   app.component = {};
 
   // display training module to user
   TrainingModule.getTrainingModule(training_id).then(function(data) {
     // if data exists, bind to view
     if(data) {
-
+      console.log(data);
       // get generic training module info to display to user
       app.title = data.data.trainingmodule.name;
       app.author = data.data.trainingmodule.author[0].name;
-      app.lastEdit = new Date(data.data.trainingmodule.last_edit).toDateString();
+      app.lastEdit = new Date(data.data.trainingmodule.lastEdit).toDateString();
 
       // get the list of components for training module
       app.components = data.data.trainingmodule.components;
 
+      // set the page index to 0, always start on first component
+      app.page = 0;
+
       // get the first training component from the training module
-      app.component = data.data.trainingmodule.components[0];
+      app.component = data.data.trainingmodule.components[app.page];
 
     }
     // if error occurs, render error message and re-direct to menu
@@ -47,12 +66,13 @@ angular.module('trainController', ['trainingServices'])
   this.nextPage = function() {
     // if components are defined
     if(app.components) {
-      if(app.component.page < app.components.length) {
-        // note - we are not adding 1 to component.page because it is based on index 1
-        // and the components array is based on index 0
-        app.component = app.components[app.component.page];
-        console.log('page incremented to ' + app.component.page);
-        console.log('new component data is' + JSON.stringify(app.component));
+      if(app.page < app.components.length) {
+        // get next component from components array
+        app.page += 1;
+        app.component = app.components[app.page];
+        console.log('page incremented to ' + app.page);
+        // check for media file and assign to scope
+        app.loadMedia(app.component);
       }
     }
     // else page is not defined we have an error
@@ -67,11 +87,13 @@ angular.module('trainController', ['trainingServices'])
   this.previousPage = function() {
     // if components are defined
     if(app.components) {
-      if(app.component.page > 1) {
-        // note - we decrement 2 to move 1 page down because component.page is based on index 1
-        app.component = app.components[app.component.page - 2];
-        console.log('page decremented to ' + app.component.page);
-        console.log('new component data is' + JSON.stringify(app.component));
+      if(app.page > 0) {
+        app.page -= 1;
+        // get previous component from components array
+        app.component = app.components[app.page];
+        console.log('page decremented to ' + app.page);
+        // check for media file and assign to scope
+        app.loadMedia(app.component);
       }
     }
     // else page is not defined we have an error
