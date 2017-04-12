619bbd822de25eca21cc5fefedbe37b86cb5e81b
Justin Johnson <johnsonj561@gmail.com>
2017-04-11 22:08:42 -0400

    getUserTraining(id) added to retrieve all of current users assignments

--- public/app/services/trainingServices.js
+++ public/app/services/trainingServices.js
@@ -5,8 +5,14 @@ angular.module('trainingServices', [])
   
   var trainingFactory = {}; // Create the User object
 
+  // get the training module with given id
   trainingFactory.getTrainingModule = function(id) {
     return $http.get('/api/trainingmodule/' + id);         
+  };
+  
+  // get the training modules assigned to user with id
+  trainingFactory.getUserTraining = function(id) {
+    return $http.get('/api/usertraining/' + id);
   };
 
 
