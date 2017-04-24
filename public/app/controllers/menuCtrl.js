d07cb7ea96829575bc00b7d0d5a3540965bac3a0
Justin Johnson <johnsonj561@gmail.com>
2017-04-23 22:59:38 -0400

    completion rate calculation added to update progress bar

--- public/app/controllers/menuCtrl.js
+++ public/app/controllers/menuCtrl.js
@@ -6,7 +6,35 @@ angular.module('menuController', ['userServices', 'authServices'])
   app = this;
   app.displayProgressBar = true;
 
-
+  /*
+  * Get user's completion rate and set progress bar
+  */
+  User.getCompletionRate().then(function(data) {
+    if(data.data.success) {
+      app.completionRate = data.data.completionRate;
+      $('div.progress-bar').width(app.completionRate + '%');
+      if(app.completionRate == 100){
+        app.progressBarMessage = 'Congrats, you are currently up to date on training.';
+      }
+      else if(app.completionRate > 80) {
+        app.progressBarMessage = 'You are doing well, almost all caught up!';
+      }
+      else if(app.completionRate > 50) {
+        app.progressBarMessage = 'You\'ve got some work to do!';
+      }
+      else if(app.completionRate > 10) {
+        app.progressBarMessage = 'Time to get busy, you are starting to fall behind!';
+      }
+      else {
+        app.progressBarMessage = 'Dude - what are you doing with your life!?';
+      }
+    }
+    else {
+      // error calculating completion rate, hide the progress bar
+      app.displayProgressBar = false;
+    }
+  });
+  
   /*
   * Get permission of current user
   */
@@ -38,7 +66,6 @@ angular.module('menuController', ['userServices', 'authServices'])
     }
     return false;
   }
-
-
+  
 
 });
