e6cb65aaafd0d3d06be7d3c4affcbb60559c14af
Justin Johnson <johnsonj561@gmail.com>
2017-04-25 21:10:14 -0400

    removed progress bar when 0 assignments exist

--- public/app/controllers/menuCtrl.js
+++ public/app/controllers/menuCtrl.js
@@ -4,49 +4,54 @@ angular.module('menuController', ['userServices', 'authServices'])
   .controller('menuCtrl', function(User, Auth) {
 
   app = this;
-  app.displayProgressBar = true;
 
-  /*
-  * Get user's completion rate and set progress bar
-  */
-  User.getCompletionRate().then(function(data) {
-    if(data.data.success) {
-      app.completionRate = data.data.completionRate;
-      $('div.progress-bar').width(app.completionRate + '%');
-      if(app.completionRate == 100){
-        app.progressBarMessage = 'Congrats, you are currently up to date on training.';
-      }
-      else if(app.completionRate > 80) {
-        app.progressBarMessage = 'You are doing well, almost all caught up!';
-      }
-      else if(app.completionRate > 50) {
-        app.progressBarMessage = 'You\'ve got some work to do!';
-      }
-      else if(app.completionRate > 10) {
-        app.progressBarMessage = 'Time to get busy, you are starting to fall behind!';
-      }
-      else {
-        app.progressBarMessage = 'Dude - what are you doing with your life!?';
-      }
-    }
-    else {
-      // error calculating completion rate, hide the progress bar
-      app.displayProgressBar = false;
-    }
-  });
-  
   /*
   * Get permission of current user
   */
   User.getPermission().then(function(data) {
     if(data.data.success) {
       app.permission = data.data.permission;
-      if(app.permission === 'user') {
-        app.displayProgressBar = true;
-      }
     }
     else {
       app.permission = false;
+    }
+  });
+
+  /*
+  * Get user's completion rate and set progress bar
+  */
+  User.getCompletionRate().then(function(data) {
+    if(data.data.success) {
+      // make sure there are no assignments first
+      if(data.data.assignmentCount == 0) {
+        app.progressBarMessage = 'You have not been assigned to any training modules yet.';
+        app.displayProgressBar = false;
+      }
+      // else there are assignments, calculate the completion rate
+      else {
+        app.displayProgressBar = true;
+        app.completionRate = data.data.completionRate;
+        $('div.progress-bar').width(app.completionRate + '%');
+        if(app.completionRate == 100){
+          app.progressBarMessage = 'Congrats, you are currently up to date on training.';
+        }
+        else if(app.completionRate > 80) {
+          app.progressBarMessage = 'You are doing well, almost all caught up!';
+        }
+        else if(app.completionRate > 50) {
+          app.progressBarMessage = 'You\'ve got some work to do!';
+        }
+        else if(app.completionRate > 10) {
+          app.progressBarMessage = 'Time to get busy, you are starting to fall behind!';
+        }
+        else {
+          app.progressBarMessage = 'Dude - what are you doing with your life!?';
+        }
+      }
+    }
+    else {
+      // error calculating completion rate, hide the progress bar
+      app.displayProgressBar = false;
     }
   });
 
@@ -56,7 +61,7 @@ angular.module('menuController', ['userServices', 'authServices'])
   app.isLoggedIn = function() {
     return Auth.isLoggedIn();
   }
-  
+
   /*
   * Return true if current user is admin
   */
@@ -66,6 +71,6 @@ angular.module('menuController', ['userServices', 'authServices'])
     }
     return false;
   }
-  
+
 
 });
