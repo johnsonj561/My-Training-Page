130f89f074c9bcc19892971ff07c6601f7073f48
Justin Johnson <johnsonj561@gmail.com>
2017-04-28 22:59:57 -0400

    get training modules by username added

--- public/app/services/trainingServices.js
+++ public/app/services/trainingServices.js
@@ -1,30 +1,36 @@
 angular.module('trainingServices', [])
 
   .factory('TrainingModule', function($http) {
-  
-  
+
+
   var trainingFactory = {}; // Create the User object
 
   // get the training module with given id
   trainingFactory.getTrainingModule = function(id) {
     return $http.get('/api/trainingmodule/' + id);         
   };
-  
+
   // get the training modules assigned to user with id
   trainingFactory.getUserTraining = function(id) {
     return $http.get('/api/usertraining/' + id);
   };
 
+  // get the training modules assigned to user with id
+  trainingFactory.getUserTrainingByUsername = function() {
+    return $http.get('/api/usertraining');
+  };
+
+
   // get all training modules available to admin
   trainingFactory.getTrainingModules = function() {
     return $http.get('/api/alltraining');
   }
-  
+
   // assign training module to users
   trainingFactory.assignTraining = function(assignmentData) {
     return $http.put('/api/assignTraining', assignmentData);
   }
-  
+
   // update the training module stats with new training scores
   trainingFactory.updateScores = function(trainingData) {
     return $http.put('/api/updateScores', trainingData);
