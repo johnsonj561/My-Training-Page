a07335d0e4c52849b11569cad222acc7c141bb2a
Justin Johnson <johnsonj561@gmail.com>
2017-04-28 22:57:05 -0400

    added report generating controller

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,5 @@
 angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'trainingServices', 'ngAnimate', 'mainController', 'authServices', 'emailController', 
-                           'managementController', 'menuController', 'trainController', 'ngSanitize'])
+                           'managementController', 'menuController', 'trainController', 'reportController', 'ngSanitize'])
 
 .config(function($httpProvider) {
   $httpProvider.interceptors.push('AuthInterceptors');
