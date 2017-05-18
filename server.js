afc8ef76d9c55da1a47067e9e1f3e889992f6973
Justin Johnson <johnsonj561@gmail.com>
2017-05-17 22:22:53 -0400

    environment variables added with dotenv

--- server.js
+++ server.js
@@ -5,82 +5,86 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var router = express.Router();
-var appRoutes = require('./app/routes/api')(router);
+var dotenv = require('dotenv').config();
+var appRoutes = require('./app/routes/api')(router, dotenv);
 var path = require('path');
 var passport = require('passport');
-var social = require('./app/passport/passport')(app, passport);
+var social = require('./app/passport/passport')(app, passport, dotenv);
+
 
 /*
-* MIDDLEWARE
-*/
+ * MIDDLEWARE
+ */
 /*
-* morgan
-* http request logger middleware for node.js
-* writes to console
-* https://www.npmjs.com/package/morgan
-*/
-app.use(morgan('dev')); 
+ * morgan
+ * http request logger middleware for node.js
+ * writes to console
+ * https://www.npmjs.com/package/morgan
+ */
+app.use(morgan('dev'));
 
 /*
-* body-parser
-* Parse incoming request bodies in a middleware before your handlers, 
-* available under the req.body property.
-* https://www.npmjs.com/package/body-parser
-*/
+ * body-parser
+ * Parse incoming request bodies in a middleware before your handlers, 
+ * available under the req.body property.
+ * https://www.npmjs.com/package/body-parser
+ */
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 /*
-* express.static(root, [options]
-* http://expressjs.com/en/api.html
-* Serves static files at the given location, gives front end access
-*/
+ * express.static(root, [options]
+ * http://expressjs.com/en/api.html
+ * Serves static files at the given location, gives front end access
+ */
 app.use(express.static(__dirname + '/public'));
 
 // force redirecto to http://www.
-app.use(function(req, res, next) {
-  console.log(req.headers.host);
-  console.log(req.url);
-  // 52\.89\.200\.83.*
-  if(req.headers.host.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}/i)){
-    console.log('regex match');
-    res.redirect(301, "http://localhost:3000" + req.url);
-  }
-  next();
+app.use(function (req, res, next) {
+	console.log(req.headers.host);
+	console.log(req.url);
+	// 52\.89\.200\.83.*
+	if (req.headers.host.match(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}/i)) {
+		console.log('regex match');
+		res.redirect(301, 'http://' + process.env.HOST + ':3000' + req.url);
+	}
+	next();
 });
 
+
 /*
-* Must access routes after they've been parsed
-* Order of middleware is important
-*/
+ * Must access routes after they've been parsed
+ * Order of middleware is important
+ */
 app.use('/api', appRoutes);
 
 
 /*
-* mongoose
-* http://mongoosejs.com/index.html
-*/
-var uri  = 'mongodb://localhost:27017/mytrainingpage';
+ * mongoose
+ * http://mongoosejs.com/index.html
+ */
+var uri = process.env.DB_HOST;
 var options = {
-  user: 'xxx',
-  pass: 'xxx'
+	user: process.env.DB_USER,
+	pass: process.env.DB_PASS
 }
-mongoose.connect(uri, options,  function(err) {
-  if(err) {
-    console.log("Not connected to the database: " + err);
-  }
-  else {
-    console.log("Successfully connected to MongoDB");
-  }
+mongoose.connect(uri, options, function (err) {
+	if (err) {
+		console.log("Not connected to the database: " + err);
+	} else {
+		console.log("Successfully connected to MongoDB");
+	}
 });
 
 
-app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
 
 
-app.listen(port, function() {
-  console.log("Running on " + port);
+app.listen(port, function () {
+	console.log("Running on " + port);
 });
