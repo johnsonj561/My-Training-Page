cc695f405aa67f2d4bf48ad84b02a59eca12d663
Justin Johnson <johnsonj561@gmail.com>
2017-05-26 21:18:02 -0400

    random num added to usersummary to resource to prevent cache

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,230 +1,235 @@
 var app = angular.module('appRoutes', ['ngRoute'])
 
-.config(function($routeProvider, $locationProvider) {
+	.config(function ($routeProvider, $locationProvider) {
 
-  $routeProvider // Create routes
+		$routeProvider // Create routes
 
-  // Home Route    
-  .when('/', {
-    templateUrl: 'app/views/pages/home.html',
+			// Home Route    
+			.when('/', {
+				templateUrl: 'app/views/pages/home.html',
 
-  })
+			})
 
-  // Register Route            
-  .when('/register', {
-    templateUrl: 'app/views/pages/users/register.html',
-    controller: 'regCtrl',
-    controllerAs: 'register',
-    authenticated: false
-  })
+			// Register Route            
+			.when('/register', {
+				templateUrl: 'app/views/pages/users/register.html',
+				controller: 'regCtrl',
+				controllerAs: 'register',
+				authenticated: false
+			})
 
-  // Login Route            
-  .when('/login', {
-    templateUrl: 'app/views/pages/users/login.html',
-    authenticated: false
-  })
+			// Login Route            
+			.when('/login', {
+				templateUrl: 'app/views/pages/users/login.html',
+				authenticated: false
+			})
 
-  // Logout Route
-  .when('/logout', {
-    templateUrl: 'app/views/pages/users/logout.html',
-    authenticated: true
-  })
+			// Logout Route
+			.when('/logout', {
+				templateUrl: 'app/views/pages/users/logout.html',
+				authenticated: true
+			})
 
-  // Use profile Route
-  .when('/profile', {
-    templateUrl: 'app/views/pages/users/profile.html',
-    authenticated: true
-  })
-  
-  .when('/editprofile', {
-    templateUrl: 'app/views/pages/users/editprofile.html',
-    controller: 'profileCtrl',
-    controllerAs: 'profile',
-    authenticated: true
-  })
+			// Use profile Route
+			.when('/profile', {
+				templateUrl: 'app/views/pages/users/profile.html',
+				authenticated: true
+			})
 
-  .when('/facebook/:token', {
-    templateUrl: 'app/views/pages/users/social/social.html',
-    controller: 'facebookCtrl',
-    controllerAs: 'facebook',
-    authenticated: false
-  })
+			.when('/editprofile', {
+				templateUrl: 'app/views/pages/users/editprofile.html',
+				controller: 'profileCtrl',
+				controllerAs: 'profile',
+				authenticated: true
+			})
 
-
-  .when('/facebookerror', {
-    templateUrl: 'app/views/pages/home.html',
-    controller: 'facebookCtrl',
-    controllerAs: 'facebook',
-    authenticated: false
-  })
-
-  .when('/facebook/inactive/error', {
-    templateUrl: 'app/views/pages/home.html',
-    controller: 'facebookCtrl',
-    controllerAs: 'facebook',
-    authenticated: false
-  })
-
-  .when('/google/:token', {
-    templateUrl: 'app/views/pages/users/social/social.html',
-    controller: 'googleCtrl',
-    controllerAs: 'google',
-    authenticated: false
-  })
-
-  .when('/googleerror', {
-    templateUrl: 'app/views/pages/home.html',
-    controller: 'googleCtrl',
-    controllerAs: 'google',
-    authenticated: false
-  })
+			.when('/facebook/:token', {
+				templateUrl: 'app/views/pages/users/social/social.html',
+				controller: 'facebookCtrl',
+				controllerAs: 'facebook',
+				authenticated: false
+			})
 
 
-  .when('/google/inactive/error', {
-    templateUrl: 'app/views/pages/home.html',
-    controller: 'googleCtrl',
-    controllerAs: 'google',
-    authenticated: false
-  })
+			.when('/facebookerror', {
+				templateUrl: 'app/views/pages/home.html',
+				controller: 'facebookCtrl',
+				controllerAs: 'facebook',
+				authenticated: false
+			})
+
+			.when('/facebook/inactive/error', {
+				templateUrl: 'app/views/pages/home.html',
+				controller: 'facebookCtrl',
+				controllerAs: 'facebook',
+				authenticated: false
+			})
+
+			.when('/google/:token', {
+				templateUrl: 'app/views/pages/users/social/social.html',
+				controller: 'googleCtrl',
+				controllerAs: 'google',
+				authenticated: false
+			})
+
+			.when('/googleerror', {
+				templateUrl: 'app/views/pages/home.html',
+				controller: 'googleCtrl',
+				controllerAs: 'google',
+				authenticated: false
+			})
 
 
-  .when('/activate/:token', {
-    templateUrl: 'app/views/pages/users/activation/activate.html',
-    controller: 'emailCtrl',
-    controllerAs: 'email'
-  })
+			.when('/google/inactive/error', {
+				templateUrl: 'app/views/pages/home.html',
+				controller: 'googleCtrl',
+				controllerAs: 'google',
+				authenticated: false
+			})
 
-  .when('/resend', {
-    templateUrl: 'app/views/pages/users/activation/resend.html',
-    controller: 'resendCtrl',
-    controllerAs: 'resend'
-  })
 
-  .when('/resetusername', {
-    templateUrl: 'app/views/pages/users/reset/username.html',
-    controller: 'usernameCtrl',
-    controllerAs: 'username',
-    authenticated: false
-  })
+			.when('/activate/:token', {
+				templateUrl: 'app/views/pages/users/activation/activate.html',
+				controller: 'emailCtrl',
+				controllerAs: 'email'
+			})
 
-  .when('/resetpassword', {
-    templateUrl: 'app/views/pages/users/reset/password.html',
-    controller: 'passwordCtrl',
-    controllerAs: 'password',
-    authenticated: false
-  })
+			.when('/resend', {
+				templateUrl: 'app/views/pages/users/activation/resend.html',
+				controller: 'resendCtrl',
+				controllerAs: 'resend'
+			})
 
-  .when('/reset/:token', {
-    templateUrl: 'app/views/pages/users/reset/newpassword.html',
-    controller: 'resetCtrl',
-    controllerAs: 'reset',
-    authenticated: false
-  })
-  
-  .when('/management', {
-    templateUrl: 'app/views/pages/management/management.html',
-    controller: 'managementCtrl',
-    controllerAs: 'management',
-    authenticated: true,
-    permission: ['admin']
-  })
+			.when('/resetusername', {
+				templateUrl: 'app/views/pages/users/reset/username.html',
+				controller: 'usernameCtrl',
+				controllerAs: 'username',
+				authenticated: false
+			})
 
-    .when('/edit/:id', {
-    templateUrl: 'app/views/pages/management/edit.html',
-    controller: 'editCtrl',
-    controllerAs: 'edit',
-    authenticated: true,
-    permission: ['admin']
-  })
-  
-  .when('/search', {
-    templateUrl: 'app/views/pages/management/search.html',
-    controller: 'managementCtrl',
-    controllerAs: 'management',
-    authenticated: true,
-    permission:['admin']
-  })
-  
-  /*
-  * User menu displays available functionality to user
-  * Should be default view when user is logged in
-  */
-  .when('/menu', {
-    templateUrl: 'app/views/pages/users/menu/menu.html',
-    controller: 'menuCtrl',
-    controllerAs: 'menu',
-    authenticated: true
-  })
-  
-  .when('/train/:id', {
-    templateUrl: 'app/views/pages/training/train.html',
-    controller: 'completeTrainingCtrl',
-    controllerAs: 'training',
-    authenticated: true
-  })
-  
-  .when('/trainingmenu', {
-    templateUrl: 'app/views/pages/training/trainingmenu.html',
-    controller: 'trainCtrl',
-    controllerAs: 'training',
-    authenticated: true
-  })
-  
-  .when('/assigntraining', {
-    templateUrl: 'app/views/pages/management/assigntraining.html',
-    controller: 'assignTrainingCtrl',
-    controllerAs: 'assign',
-    authenticated: true
-  })
-  
-  .when('/usersummary', {
-    templateUrl: 'app/views/pages/users/reports/summary.html',
-    controller: 'reportCtrl',
-    controllerAs: 'report',
-    authenticated: true
-  })
-  
-  
-  // "catch all" to redirect to home page            
-  .otherwise({ redirectTo: '/' });
+			.when('/resetpassword', {
+				templateUrl: 'app/views/pages/users/reset/password.html',
+				controller: 'passwordCtrl',
+				controllerAs: 'password',
+				authenticated: false
+			})
 
-  // Required for no base (remove '#' from address bar)
-  $locationProvider.html5Mode({ enabled: true, requireBase: false });
-});
+			.when('/reset/:token', {
+				templateUrl: 'app/views/pages/users/reset/newpassword.html',
+				controller: 'resetCtrl',
+				controllerAs: 'reset',
+				authenticated: false
+			})
+
+			.when('/management', {
+				templateUrl: 'app/views/pages/management/management.html',
+				controller: 'managementCtrl',
+				controllerAs: 'management',
+				authenticated: true,
+				permission: ['admin']
+			})
+
+			.when('/edit/:id', {
+				templateUrl: 'app/views/pages/management/edit.html',
+				controller: 'editCtrl',
+				controllerAs: 'edit',
+				authenticated: true,
+				permission: ['admin']
+			})
+
+			.when('/search', {
+				templateUrl: 'app/views/pages/management/search.html',
+				controller: 'managementCtrl',
+				controllerAs: 'management',
+				authenticated: true,
+				permission: ['admin']
+			})
+
+			/*
+			 * User menu displays available functionality to user
+			 * Should be default view when user is logged in
+			 */
+			.when('/menu', {
+				templateUrl: 'app/views/pages/users/menu/menu.html',
+				controller: 'menuCtrl',
+				controllerAs: 'menu',
+				authenticated: true
+			})
+
+			.when('/train/:id', {
+				templateUrl: 'app/views/pages/training/train.html',
+				controller: 'completeTrainingCtrl',
+				controllerAs: 'training',
+				authenticated: true
+			})
+
+			.when('/trainingmenu', {
+				templateUrl: 'app/views/pages/training/trainingmenu.html',
+				controller: 'trainCtrl',
+				controllerAs: 'training',
+				authenticated: true
+			})
+
+			.when('/assigntraining', {
+				templateUrl: 'app/views/pages/management/assigntraining.html',
+				controller: 'assignTrainingCtrl',
+				controllerAs: 'assign',
+				authenticated: true
+			})
+
+			.when('/usersummary', {
+				templateUrl: 'app/views/pages/users/reports/summary.html?' + Math.random().toFixed(5),
+				controller: 'reportCtrl',
+				controllerAs: 'report',
+				authenticated: true
+			})
+
+
+			// "catch all" to redirect to home page            
+			.otherwise({
+				redirectTo: '/'
+			});
+
+		// Required for no base (remove '#' from address bar)
+		$locationProvider.html5Mode({
+			enabled: true,
+			requireBase: false
+		});
+	});
 
 
 // Handle user authentication errors
-app.run(['$rootScope', 'Auth', '$location', 'User', function($rootScope, Auth, $location, User) {
-  $rootScope.$on('$routeChangeStart', function(event, next, current) {
+app.run(['$rootScope', 'Auth', '$location', 'User', function ($rootScope, Auth, $location, User) {
+	$rootScope.$on('$routeChangeStart', function (event, next, current) {
 
-    if(next.$$route !== undefined) {
+		if (next.$$route !== undefined) {
 
-      if(next.$$route.authenticated == true) {
-        if(!Auth.isLoggedIn()) {
-          // prevent user from viewing page that requires authentication
-          event.preventDefault();
-          $location.path('/');
-        }
-        else if(next.$$route.permission) {
+			if (next.$$route.authenticated == true) {
+				if (!Auth.isLoggedIn()) {
+					// prevent user from viewing page that requires authentication
+					event.preventDefault();
+					$location.path('/');
+				} else if (next.$$route.permission) {
 
-          User.getPermission().then(function(data) {
+					User.getPermission().then(function (data) {
 
-            if(next.$$route.permission[0] !== data.data.permission) {
-              if(next.$$route.permission[1] !== data.data.permission) {
-                event.preventDefault();
-                $location.path('/');
-              }
-            }
-          });
-        }
-      }
-      // this line does not make sense - it looks like the same statement above
-      // equiv of next.$$route.authenticated == true
-      else if(!next.$$route.authenticated == false) {
-        if(Auth.isLoggedIn()) {
-          event.preventDefault();
-          $location.path('/profile');
-        }
-      };
-    }
-  })}]);
+						if (next.$$route.permission[0] !== data.data.permission) {
+							if (next.$$route.permission[1] !== data.data.permission) {
+								event.preventDefault();
+								$location.path('/');
+							}
+						}
+					});
+				}
+			}
+			// this line does not make sense - it looks like the same statement above
+			// equiv of next.$$route.authenticated == true
+			else if (!next.$$route.authenticated == false) {
+				if (Auth.isLoggedIn()) {
+					event.preventDefault();
+					$location.path('/profile');
+				}
+			};
+		}
+	})
+}]);
